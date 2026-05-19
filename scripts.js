5f689bc870dcc35fef370b3d63e6512c4a8bde02
Hideki <lucashidekitogo0227@gmail.com>
2026-05-19 19:54:50 -0300

    Código completo de busca de CEP pt2

--- scripts.js
+++ scripts.js
@@ -7,12 +7,36 @@ const cidade = document.getElementById("cidade");
 const estado = document.getElementById("estado");
 
 cep.addEventListener("blur", async () => {
-    
-    const cepLimpo = cep.value.replace(/\D/g, "");
 
-    if (cepLimpo.length !== 8) {
-  alert("CEP inválido");
-  return;
+  const cepLimpo = cep.value.replace(/\D/g, "");
+
+  if (cepLimpo.length !== 8) {
+    alert("CEP inválido");
+    return;
+  }
+
+  try {
+
+    const response = await fetch(
+      `https://viacep.com.br/ws/${cepLimpo}/json/`
+    );
+
+    const data = await response.json();
+
+    if (data.erro) {
+      alert("CEP não encontrado");
+      return;
+    }
+
+    rua.value = data.logradouro;
+    bairro.value = data.bairro;
+    cidade.value = data.localidade;
+    estado.value = data.uf;
+
+  } catch (error) {
+
+    console.log("Erro ao buscar CEP", error);
+
   }
 
 });
