6769addd694d7109919145f9d693e0437eff8564
Hideki <lucashidekitogo0227@gmail.com>
2026-05-19 19:39:01 -0300

    Pegar os inputs do HTML

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const nome = document.getElementById("nome");
+const email = document.getElementById("email");
+const cep = document.getElementById("cep");
+const rua = document.getElementById("rua");
+const bairro = document.getElementById("bairro");
+const cidade = document.getElementById("cidade");
+const estado = document.getElementById("estado");
